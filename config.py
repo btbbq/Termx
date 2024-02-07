e1817b805352e0f4d03c6ff7782b364f3e8356e9
btbbq <145179751+btbbq@users.noreply.github.com>
2024-02-07 19:53:46 +0400

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import os
 class Config(object):
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
-    APP_ID = int(os.environ.get("APP_ID", 10215855))
+    APP_ID = int(os.environ.get("APP_ID", 26983509)
 
-    API_HASH = os.environ.get("API_HASH", "c6eac883d0f3124c86408a907bdde463)
+    API_HASH = os.environ.get("API_HASH", "ab5d4207ca2212869822f58afee6be9e)
